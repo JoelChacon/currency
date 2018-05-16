750d8da2872c009feb5be887ea18c413a5d76142
Joel G Chacon <joel.chacon@bbva.com>
2018-05-16 16:52:57 -0500

    Polymer component for formattting currency according to the currency code

--- format-currency/format-currency.js
+++ format-currency/format-currency.js
@@ -5,8 +5,49 @@ class FormatCurrency extends Polymer.Element {
   }
 
   static get properties() {
-    return {};
+    return {
+      
+      amount: {
+        type: Number,
+        value: 2000,
+      },
+
+      currencyCode: {
+        type: String,
+        observer: 'currencySymbol',
+      },
+
+      currencyPrefix: {
+        type: String,
+      },
+
+      currencySuffix: {
+        type: String,
+      },
+
+      separator: {
+        type: String,
+        value: ','
+      },
+
+      value: {
+        type: String,
+        notify: true,
+      },
+
+    };
   }
+
+  currencySymbol(code) {
+    if(code === 'US' || code === 'MXN') {
+      this.set('currencyPrefix', '$');
+      this.set('currencySuffix', '');
+    } else {
+      this.set('currencyPrefix', '');
+      this.set('currencySuffix', '€');     
+    }
+  }
+
 }
 
 customElements.define(FormatCurrency.is, FormatCurrency);
